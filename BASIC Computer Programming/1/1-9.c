4515f811fd7b946bdd3574267be86db9f1527acb
Thandon-To <thanadonlungta@gmail.com>
2025-11-29 14:45:50 +0700

    Update 1-9.c

--- BASIC Computer Programming/1/1-9.c
+++ BASIC Computer Programming/1/1-9.c
@@ -1,43 +1,40 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main() {
-    int ZoneCode;
+    int zoneCode;
     float weight_kg;
-    float totalCost;
-
-    if (scanf("%d %f", &ZoneCode, &weight_kg) != 2) {
-        return 1;
-    } // end if
-
-    switch (ZoneCode)
-    {
-    case 1:
-        if (weight_kg <= 5) {
-            totalCost = 50; 
-        } else if (weight_kg > 5) {
-            totalCost = 80; 
-        }
-        break;
-    case 2:
-        if (weight_kg <= 10) {
-            totalCost = 150; 
-        } else if (weight_kg > 10) {
-            totalCost = 250; 
-        }
-        break;
-    case 3:
-        totalCost = 500; 
-        break;
+    float totalCost = 0.0;
     
-    default:
-    printf("Invalid Zone Code");
-        break;
+    if (scanf("%d %f", &zoneCode, &weight_kg) != 2) {
+        return 1; 
+    }
+    
+    switch (zoneCode) {
+        case 1:
+            if (weight_kg <= 5) {
+                totalCost = 50;
+            } else {
+                totalCost = 80;
+            }
+            break;
+        case 2:
+            if (weight_kg <= 10) {
+                totalCost = 150;
+            } else {
+                totalCost = 250;
+            }
+            break;
+        case 3:
+            totalCost = 500;
+            break;
+        default:
+            printf("Invalid Zone Code\n");
+            break;
     }
 
-    if (ZoneCode == 1 && ZoneCode == 2 && ZoneCode == 3)
-    {
+    if (totalCost > 0.0 || zoneCode > 3 || zoneCode < 1) { 
         printf("%.2f\n", totalCost);
-    }
-    
+    } 
+
     return 0;
 }
