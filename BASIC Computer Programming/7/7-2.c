d82e7f70ef7e54807cd5642d15ebbe94970e22fa
Thandon-To <thanadonlungta@gmail.com>
2025-12-30 21:18:57 +0700

    Refactor program to read data from CSV file
    Replaced rectangle area calculation with file reading and data analysis.

--- BASIC Computer Programming/7/7-2.c
+++ BASIC Computer Programming/7/7-2.c
@@ -1,28 +1,27 @@
 #include <stdio.h>
 
-int calculateArea(int length, int width);
-
-int calculateArea(int length, int width) {
-    int area;
-    area = length * width;
-    return area;
-}
-
 int main() {
-    int length, width, area;
+    FILE *fptr;
+    const char *FILENAME = "input_data.csv";
+    int num1, num2, num3, num4, num5;
+    int total_sum;
 
-    printf("Enter rectangle length (integer): ");
-    scanf("%d", &length);
+    fptr = fopen(FILENAME, "r");
 
-    printf("Enter rectangle width (integer): ");
-    scanf("%d", &width);
+    if (fptr == NULL) {
+        printf("ERROR: Could not open file %s for reading. Make sure the file exists.\n", FILENAME);
+    }
 
-    area = calculateArea(length, width);
+    printf("--- FILE READING PROCESS --\n");
+    printf("Reading data from %s...\n", FILENAME);
 
-    printf("\n--- RECTANGLE AREA REPORT ---\n");
-    printf("Length: %d\n", length);
-    printf("width: %d\n", width);
-    printf("Calculated Area: %d\n", area);
+    fscanf(fptr, "%d,%d,%d,%d,%d", &num1, &num2, &num3, &num4, &num5);
+    fclose(fptr);
 
-    return 0;
+    total_sum = num1 + num2 + num3 + num4 + num5;
+
+    printf("File reading complete.\n");
+    printf("\n--- DATA ANALYSIS REPORT ---\n");
+    printf("Data read: %d, %d, %d, %d, %d\n", num1, num2, num3, num4, num5);
+    printf("Total Sum: %d \n", total_sum);
 }
